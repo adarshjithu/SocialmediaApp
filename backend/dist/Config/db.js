95441fd69429d9e05add39dff0d24f6a87addf60
adarsh <adarshjithu10@gmail.com>
2024-10-13 20:58:35 +0530

    updated mongodb

--- backend/dist/Config/db.js
+++ backend/dist/Config/db.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose_1.default.connect(`${process.env.MONGODB_CONNECTION_STRING}`, {});
+        yield mongoose_1.default.connect(`${'mongodb+srv://adarshjithu10:gnRFd0XoU2bJGXko@cluster0.xhehb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'}`, {});
         console.log('MongoDB connected...');
     }
     catch (err) {
