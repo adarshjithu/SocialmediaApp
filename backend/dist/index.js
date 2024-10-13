586b4c067d4c66cc6b14a5cb9f34002295b0ed67
adarsh <adarshjithu10@gmail.com>
2024-10-13 21:22:59 +0530

    changing config

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = require("./app"); // Import the server
 const db_1 = __importDefault(require("./Config/db"));
-dotenv_1.default.config();
+dotenv_1.default.config({ path: '../.env' });
 (0, db_1.default)();
 app_1.server.listen(3000, () => {
     console.log('Server started on port 3000');
